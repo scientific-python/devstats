5c553b743c55e707d85f40274a430491e7f8f654
Stefan van der Walt <stefanv@berkeley.edu>
2023-05-25 17:20:16 -0700

    Split publishing into template / publish commands (#16)

--- devstats/publish.py
+++ devstats/publish.py
@@ -1,31 +1,77 @@
 import os
 import sys
 from glob import glob
+import shutil
+import re
+import functools
+
 import click
 
 
 @click.command()
 @click.argument("project")
 @click.option(
-    "-o", "--outdir", default="build", help="Output directory", show_default=True
+    "-o", "--outdir", default="source", help="Output directory", show_default=True
 )
-def publish(project, outdir):
-    """Generate myst report for `repo_owner`/`repo_name`."""
+def template(project, outdir):
+    """Generate myst report template for `repo_owner`/`repo_name`"""
     os.makedirs(outdir, exist_ok=True)
     os.makedirs(os.path.join(outdir, project), exist_ok=True)
 
+    print(f"Populating [{outdir}] with templates for [{project}] report:", flush=True)
+
     report_files = glob(os.path.join(os.path.dirname(__file__), "reports/*.md"))
+    for f in report_files:
+        dest = f"{outdir}/{project}/{os.path.basename(f)}"
+        print(f" - {dest}")
+        shutil.copyfile(f, dest)
+
+
+def _include_file(basedir, x):
+    fn = x.group(1)
+    with open(os.path.join(basedir, fn)) as f:
+        return f.read()
+
+
+@click.command()
+@click.argument("project")
+@click.option(
+    "-t",
+    "--templatedir",
+    default="source",
+    help="Template directory",
+    show_default=True,
+)
+@click.option(
+    "-o", "--outdir", default="build", help="Output directory", show_default=True
+)
+def publish(project, templatedir, outdir):
+    """Compile templates into markdown files ready for sphinx / myst"""
+    os.makedirs(outdir, exist_ok=True)
+    os.makedirs(os.path.join(outdir, project), exist_ok=True)
 
     variables = {"project": project}
 
-    print(f"Generating [{project}] report in [{outdir}]...", end="", flush=True)
+    print(f"Templating [{project}] report from [{templatedir}] to [{outdir}]...")
 
-    for report in report_files:
-        with open(report) as fh:
+    template_files = glob(f"{templatedir}/{project}/*.md")
+
+    for f in template_files:
+        with open(f) as fh:
             template = fh.read()
-        with open(f"{outdir}/{project}/{os.path.basename(report)}", "w") as fh:
+        dest_dir = f"{outdir}/{project}"
+        dest = f"{dest_dir}/{os.path.basename(f)}"
+        with open(dest, "w") as fh:
+            print(f" - {dest}")
             for v in variables:
                 template = template.replace("{{ " + v + " }}", variables[v])
-            fh.write(template)
 
-    print("OK")
+            # Handle myst includes
+            template = re.sub(
+                r"```{include}\s*(.*)\s*```",
+                functools.partial(_include_file, dest_dir),
+                template,
+                flags=re.MULTILINE,
+            )
+
+            fh.write(template)
